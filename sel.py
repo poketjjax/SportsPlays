cc02b642f07077a4c85ea60f3287cb9d23f04c83
Jaxon <jackson.wright@yahoo.com>
2016-06-06 08:39:24 -0500

    adding local file for 1st commit

---
+++ sel.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+#driver.get('http://contests.covers.com/Survivor/Contestant/d48ff985-065e-e511-9e61-0024e8753722')
+driver.get('http://contests.covers.com/Survivor/Contestant/5493f985-065e-e511-9e61-0024e8753722')
+
+searchBox = driver.find_element_by_id('txtSearch')
+
+if "Next Pick Deadline" in driver.page_source:
+	driver.quit()
+else:	
+	league = driver.find_element_by_css_selector('table.cmg_contests_pendingpicks tr:nth-child(2) td:nth-child(2)').text
+	team = driver.find_element_by_css_selector('table.cmg_contests_pendingpicks tr:nth-child(2) td:nth-child(3)').text[:6]
+	bet = driver.find_element_by_css_selector('table.cmg_contests_pendingpicks tr:nth-child(2) td:nth-child(6)').text
+	searchBox.send_keys(bet)
+
+	driver.get('http://www.sportsplays.com/login.html')
+	
+	username = driver.find_element_by_id('username')
+	username.send_keys('')
+	
+	password = driver.find_element_by_id('password')
+	password.send_keys('' + '\n')
+	
+	time.sleep(5)
+	driver.get('http://www.sportsplays.com/make-pick.html')
+	
+	driver.find_element_by_link_text(league).click()
+
+time.sleep(5)
+driver.quit()
