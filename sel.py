e022e66763afb5f5ed2370bca027a4281b91b5d3
Jaxon <jackson.wright@yahoo.com>
2016-06-08 08:38:23 -0500

    using config file for username and password

--- sel.py
+++ sel.py
@@ -3,8 +3,8 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-#driver.get('http://contests.covers.com/Survivor/Contestant/d48ff985-065e-e511-9e61-0024e8753722')
-driver.get('http://contests.covers.com/Survivor/Contestant/5493f985-065e-e511-9e61-0024e8753722')
+driver.get('http://contests.covers.com/Survivor/Contestant/d48ff985-065e-e511-9e61-0024e8753722')
+#driver.get('http://contests.covers.com/Survivor/Contestant/5493f985-065e-e511-9e61-0024e8753722')
 
 searchBox = driver.find_element_by_id('txtSearch')
 
@@ -18,11 +18,15 @@ else:
 
 	driver.get('http://www.sportsplays.com/login.html')
 	
+	config = open('../config.txt', 'r')
+	
 	username = driver.find_element_by_id('username')
-	username.send_keys('')
+	username.send_keys(config.readline())
 	
 	password = driver.find_element_by_id('password')
-	password.send_keys('' + '\n')
+	password.send_keys(config.readline() + '\n')
+	
+	config.close()
 	
 	time.sleep(5)
 	driver.get('http://www.sportsplays.com/make-pick.html')
